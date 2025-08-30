17fc2ca5efe0e21ae44f53811b9f5ec34fd2c731
Dired <iddersidien@ymail.com>
2025-08-30 09:19:48 +0200

    modified width on paragraph and advantage seçtion

--- source/Home/shopSection.js
+++ source/Home/shopSection.js
@@ -53,28 +53,28 @@ function ServiceAdvantageList(props){
     return (
         
             <div className="flex flex-row justify-between overflow-x-auto   xl:px-[5%] mb-20 ">
-                    <div className="flex flex-row advantageCard flex-none   xl:w-[20vw] lg:w-[25vw] w-[60vw] mr-8  ">      
+                    <div className="flex flex-row advantageCard flex-none   xl:w-[20vw] lg:w-[25vw] md:w-[40vw] w-[60vw] mr-8  ">      
                         <div className="  aspect-square flex-none  xl:w-[4vw] lg:w-[5vw] md:w-[7vw] w-[15vw] bg-[url(/truck.png)] mr-4  bg-center bg-contain bg-no-repeat  "></div>
                         <div className="flex flex-col  flex-none lg:justify-center py-2  h-[7vw]">
                             <h3 className="font-medium text-lg">Free Shipping</h3>
                             <p>On order over $100</p>
                         </div>
                     </div>
-                    <div className="flex flex-row advantageCard flex-none   xl:w-[20vw] lg:w-[25vw] w-[60vw] mr-8  ">
+                    <div className="flex flex-row advantageCard flex-none   xl:w-[20vw] lg:w-[25vw] md:w-[40vw] w-[70vw] mr-8  ">
                         <div className=" aspect-square flex-none  xl:w-[4vw] lg:w-[5vw] md:w-[7vw] w-[15vw] bg-[url(/box.png)] mr-4  bg-center bg-contain bg-no-repeat "></div>
                         <div className="flex flex-col  flex-none lg:justify-center py-2  h-[7vw]">
                             <h3 className="font-medium text-lg">Flexible & Easy Return</h3>
                             <p>Return within 14 days</p>
                         </div>
                     </div>
-                    <div className="flex flex-row advantageCard flex-none   xl:w-[20vw] lg:w-[25vw] w-[60vw] mr-8 ">
+                    <div className="flex flex-row advantageCard flex-none   xl:w-[20vw] lg:w-[25vw] md:w-[40vw] w-[70vw] mr-8 ">
                         <div className=" aspect-square flex-none  xl:w-[4vw] lg:w-[5vw] md:w-[7vw] w-[15vw] bg-[url(/phone.png)] mr-4  bg-center bg-contain bg-no-repeat "></div>
                         <div className="flex flex-col  flex-none lg:justify-center py-2  h-[7vw]">
                             <h3 className="font-medium text-lg">24/7 Support Services</h3>
                             <p>Any Time Customer Support</p>
                         </div>
                     </div>
-                    <div className="flex flex-row advantageCard  flex-none  xl:w-[20vw] lg:w-[25vw] w-[60vw]  ">
+                    <div className="flex flex-row advantageCard  flex-none  xl:w-[20vw] lg:w-[25vw] md:w-[40vw] w-[80vw]  ">
                         <div className=" aspect-square flex-none  xl:w-[4vw] lg:w-[5vw] md:w-[7vw] w-[15vw] bg-[url(/payment.png)] mr-4  bg-center bg-contain bg-no-repeat"></div>
                         <div className="flex flex-col  flex-none lg:justify-center py-2  h-[7vw]">
                             <h3 className="font-medium text-lg">Secure payment</h3>
@@ -93,7 +93,7 @@ function TrendOfTheSeason(props){
             <div className="flex md:flex-row md:justify-between flex-col  md:items-center mb-20  xl:px-[5%] lg:px-[3%] md:px-[3%] px-[5%]">
                 <div className="flex flex-col justify-between xl:w-[22%] lg:w-[27%] md:w-[35%] h-fit md:mb-0 mb-10 ">
                     <h3 className="text-2xl font-medium md:mb-6 mb-4">Trends of the Season</h3>
-                    <p className="  md:mb-6 mb-4 w-[80vw]">A never-ending rush, trendy combinations, cool steps. Check out the indispensable pieces of urban style.</p>
+                    <p className="  md:mb-6 mb-4 md:w-auto w-[80vw]">A never-ending rush, trendy combinations, cool steps. Check out the indispensable pieces of urban style.</p>
                     <button className="cursor-pointer text-white font-medium text-normal px-8 py-3 bg-[hsl(210,9%,31%)] rounded-lg w-fit hover:bg-[hsl(40,100%,50%)] hover:text-black ">Discover</button>
                 </div>
                 <ul className="flex flex-row justify-between overflow-x-auto xl:w-fit lg:w-[65vw] md:w-[60vw] w-full  ">
